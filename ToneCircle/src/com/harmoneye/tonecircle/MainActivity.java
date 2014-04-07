972aeb05e5a5aae1b8493d61408955e0dc745f81
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-04-07 15:39:27 +0200

    Show the active chord name in a separate text view.

--- ToneCircle/src/com/harmoneye/tonecircle/MainActivity.java
+++ ToneCircle/src/com/harmoneye/tonecircle/MainActivity.java
@@ -1,20 +1,18 @@
 package com.harmoneye.tonecircle;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.Menu;
 import android.widget.TextView;
-import android.widget.ToggleButton;
+
+import com.harmoneye.tonecircle.ToneCircleView.OnTonesChangedListener;
 
 public class MainActivity extends Activity {
 
-//	private TextView titleView;
 	private static final String ACTIVE_TONES = "active_tones";
 
+	private PitchClassSetNamer namer;
 	private ToneCircleView toneCircleView;
 
 	@Override
@@ -22,29 +20,20 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-//		titleView = (TextView) findViewById(R.id.title);
+		namer = PitchClassSetNamer.fromJson(getResources()
+			.openRawResource(R.raw.chord_names));
+
+		final TextView titleView = (TextView) findViewById(R.id.title);
 		toneCircleView = (ToneCircleView) findViewById(R.id.toneCircle);
 
-//		CircleLayout circle = (CircleLayout) findViewById(R.id.circleLayout);
-//
-//		for (int i = 0; i < 12; i++) {
-//			ToggleButton toggle = new ToggleButton(getApplicationContext());
-//			CharSequence title = TONE_NAMES[i];
-//			toggle.setText(title);
-//			toggle.setTextOn(title);
-//			toggle.setTextOff(title);
-//			//toggle.setBackgroundResource(R.drawable.magenta_shape);
-//			toggle.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 40);
-//			toggle.setOnCheckedChangeListener(new OnCheckedChangeListener() {
-//				@Override
-//				public void onCheckedChanged(CompoundButton buttonView,
-//					boolean isChecked) {
-//					updateChordName();
-//				}
-//			});
-//			toggles.add(toggle);
-//			circle.addView(toggle);
-//		}
+		toneCircleView.setOnTonesChangedListener(new OnTonesChangedListener() {
+			@Override
+			public void onTonesChanged(PitchClassSet activeTones) {
+				titleView.setText(namer.getName(activeTones));
+			}
+		});
+	}
+
 	@Override
 	protected void onResume() {
 		SharedPreferences prefs = getPreferences(MODE_PRIVATE);
@@ -55,15 +44,6 @@ public class MainActivity extends Activity {
 		super.onResume();
 	}
 
-//	protected void updateChordName() {
-//		StringBuilder title = new StringBuilder();
-//		for () {
-//			if (toggle.isChecked()) {
-//				title.append(toggle.getText()).append(" ");
-//			}
-//		}
-//		titleView.setText(title.toString());
-//	}
 	@Override
 	protected void onPause() {
 		int activeTones = toneCircleView.getActiveTones().getIndex();
