6e1b8ac9f64441b9677ee454d89ccfea90e59865
Bohumir Zamecnik <bohumir.zamecnik@gmail.com>
2014-04-07 22:45:27 +0200

    Modify the pitch class namer to set the names via constructor and throw a runtime exception instead of just silently continuing.

--- ToneCircle/src/com/harmoneye/tonecircle/PitchClassSetNamer.java
+++ ToneCircle/src/com/harmoneye/tonecircle/PitchClassSetNamer.java
@@ -1,10 +1,7 @@
 package com.harmoneye.tonecircle;
 
 import java.io.InputStream;
-import java.util.HashMap;
 import java.util.Map;
-
-import android.util.Log;
 
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.core.JsonParser;
@@ -13,28 +10,32 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class PitchClassSetNamer {
 
-	private Map<Integer, Map<String, Object>> names = new HashMap<Integer, Map<String, Object>>();
+	private Map<Integer, Map<String, Object>> names;
 
 	private static final String[] TONE_NAMES = { "C", "Db", "D", "Eb", "E",
 		"F", "Gb", "G", "Ab", "A", "Bb", "B" };
-	
-	private PitchClassSetNamer() {
 
+	private PitchClassSetNamer(Map<Integer, Map<String, Object>> names) {
+		this.names = names;
 	}
 
+	@SuppressWarnings("unchecked")
 	public static PitchClassSetNamer fromJson(InputStream inputStream) {
-		PitchClassSetNamer namer = new PitchClassSetNamer();
 		try {
 			JsonFactory f = new JsonFactory();
 			f.enable(JsonParser.Feature.ALLOW_COMMENTS);
 			ObjectMapper objectMapper = new ObjectMapper(f);
-			namer.names = objectMapper.readValue(inputStream,
+			Object n = objectMapper.readValue(inputStream,
 				new TypeReference<Map<Integer, Map<String, Object>>>() {
 				});
+			return new PitchClassSetNamer((Map<Integer, Map<String, Object>>) n);
 		} catch (Exception e) {
-			Log.e("Namer", e.toString());
+			if (e instanceof RuntimeException) {
+				throw (RuntimeException) e;
+			} else {
+				throw new RuntimeException(e);
+			}
 		}
-		return namer;
 	}
 
 	public String getName(PitchClassSet tones) {
@@ -46,7 +47,8 @@ public class PitchClassSetNamer {
 		if (templateRoot == null) {
 			templateRoot = 0;
 		}
-		int root = Modulo.modulo(templateRoot + tones.getRoot(), PitchClassSet.OCTAVE_SIZE);
+		int root = Modulo.modulo(templateRoot + tones.getRoot(),
+			PitchClassSet.OCTAVE_SIZE);
 		String rootName = TONE_NAMES[root];
 		String chordName = (String) details.get("chord");
 		if (chordName == null) {
